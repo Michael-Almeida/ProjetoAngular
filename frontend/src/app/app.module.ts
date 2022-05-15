dec4599c199d2e1712c1eee0fa0f2a3d591aac65
Michael Almeida <michaelcardoso97@gmail.com>
2022-05-15 20:21:46 -0300

    Navegando para telas

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,10 +15,13 @@ import {MatCardModule} from '@angular/material/card'
 import {MatListModule} from "@angular/material/list";
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import {MatButtonModule} from '@angular/material/button'
+
 
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, ProductCrudComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, ProductCrudComponent, ProductCreateComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -26,7 +29,8 @@ import { ProductCrudComponent } from './views/product-crud/product-crud.componen
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
