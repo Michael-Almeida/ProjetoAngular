c11d43833b7ebf0c41bfc37fc53be4c17d50b4ae
Michael Almeida <michaelcardoso97@gmail.com>
2022-05-21 23:18:06 -0300

    Métodos readById e update

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -30,4 +30,17 @@ export class ProductService {
   read(): Observable<Product[]> {
     return this.http.get<Product[]>(this.baseUrl);
   }
+
+  readById (id:String): Observable <Product>{
+    /* `` = template String
+       ${} - interpolação
+    */
+    const url =`${this.baseUrl}/${id}}`  
+    return this.http.get<Product>(url)
+  }
+
+  update (product: Product): Observable<Product>{
+    const url = `${this.baseUrl}/${product.id}`
+    return this.http.put<Product>(url, product)
+  }
 }
